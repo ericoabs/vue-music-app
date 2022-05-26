128a9b9eb604b2c2099735a47e67da715397bda3
Erico Alexandre <ericoabs25@gmail.com>
2022-05-25 21:52:30 -0300

    changes to env variables

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,14 +4,6 @@ import 'firebase/firestore';
 
 require('dotenv').config();
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDbH_y_SLSTqKNo2MHeQ8rNcLkYoGlkIec',
-//   authDomain: 'music-app-25172.firebaseapp.com',
-//   projectId: 'music-app-25172',
-//   storageBucket: 'music-app-25172.appspot.com',
-//   appId: '1:345080771388:web:e35dbf4e674f87f7606439',
-// };
-
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
