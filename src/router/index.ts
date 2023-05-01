325bc856b05ca800e6f233346ca97412ba6b7c94
Erico Alexandre <ericoabs25@gmail.com>
2023-05-01 20:55:56 -0300

    feature: create auth requirement to manage page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
+import useUserStore from '@/stores/user'
 
 const routes = [
   {
@@ -17,7 +18,10 @@ const routes = [
   {
     name: 'manage',
     path: '/manage-music',
-    component: Manage
+    component: Manage,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/manage',
@@ -33,6 +37,21 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+
+  const store = useUserStore()
+
+  if (store.userLoggedIn) {
+    next()
+  }
+
+  next({ name: 'home' })
 })
 
 export default router
