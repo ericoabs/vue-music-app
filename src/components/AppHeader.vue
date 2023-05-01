be977556ac7110927fd3b9cbfe53b94e19fc77dd
Erico Alexandre <ericoabs25@gmail.com>
2023-05-01 19:59:05 -0300

    feature: adding router links to music, about and manage pages

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,18 @@
 <template>
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <RouterLink
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</RouterLink
+      >
 
       <div class="flex flex-grow items-center">
         <ul class="flex flex-row mt-1">
+          <li>
+            <RouterLink class="px-2 text-white" :to="{ name: 'about' }">About</RouterLink>
+          </li>
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -12,7 +20,7 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <RouterLink class="px-2 text-white" :to="{ name: 'manage' }">Manage</RouterLink>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
