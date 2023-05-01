8830060ff0060f435c5a08061c3770aff14dabaf
Erico Alexandre <ericoabs25@gmail.com>
2023-05-01 15:58:50 -0300

    feature: create logout function and button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,14 +5,19 @@
 
       <div class="flex flex-grow items-center">
         <ul class="flex flex-row mt-1">
-          <li>
+          <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
             >
           </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
+          <template v-else>
+            <li>
+              <a class="px-2 text-white" href="#">Manage</a>
+            </li>
+            <li>
+              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+            </li>
+          </template>
         </ul>
       </div>
     </nav>
@@ -22,11 +27,12 @@
 <script lang="ts">
 import { mapStores } from 'pinia'
 import useModalStore from '@/stores/modal'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'AppHeader',
   computed: {
-    ...mapStores(useModalStore)
+    ...mapStores(useModalStore, useUserStore)
   },
   methods: {
     toggleAuthModal() {
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -75,6 +75,8 @@ export default {
 
       this.login_alert_variant = 'bg-green-500'
       this.login_alert_msg = 'Success! You are now logged in.'
+
+      window.location.reload()
     }
   }
 }
