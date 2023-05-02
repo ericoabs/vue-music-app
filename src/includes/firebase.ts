b669c450e6b7c5331ffbab723233a1d0b15e6c06
Erico Alexandre <ericoabs25@gmail.com>
2023-05-02 15:44:03 -0300

    feature: create firebase storage

--- src/includes/firebase.ts
+++ src/includes/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -15,7 +16,8 @@ firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 
 const usersCollections = db.collection('users')
 
-export { auth, db, usersCollections }
+export { auth, db, storage, usersCollections }
