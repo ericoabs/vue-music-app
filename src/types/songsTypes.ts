848d6e58320d5680df9cf8b93c4c9507750b4a61
Erico Alexandre <ericoabs25@gmail.com>
2023-05-05 20:04:23 -0300

    feature: add songs type

---
+++ src/types/songsTypes.ts
@@ -0,0 +1,10 @@
+export interface Song {
+  modified_name: string
+  uid: string
+  genre: string
+  orinal_name: string
+  display_name: string
+  comment_count: number
+  url: string
+  docID: string
+}
