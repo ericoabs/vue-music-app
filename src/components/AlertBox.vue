d7f385f4c4bfc5ea9526afabc1e4166d690571ac
Erico Alexandre <ericoabs25@gmail.com>
2023-05-11 18:23:42 -0300

    feature: create the AlertBox component

---
+++ src/components/AlertBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="showAlert" class="text-white text-center font-bold p-4 mb-4" :class="alertVariant">
+    {{ alertMessage }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AlertBox',
+  props: {
+    showAlert: {
+      type: Boolean,
+      required: true
+    },
+    alertVariant: {
+      type: String,
+      required: true
+    },
+    alertMessage: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
