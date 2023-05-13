f777982bfe235632509320692f0007d8a9d4d532
Erico Alexandre <ericoabs25@gmail.com>
2023-05-13 20:09:02 -0300

    refactor: remove comments

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -5,7 +5,6 @@
       style="background-image: url(/assets/img/song-header.png)"
     ></div>
     <div class="container mx-auto flex items-center">
-      <!-- Play/Pause Button -->
       <button
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
@@ -13,17 +12,14 @@
         <i class="fas fa-play"></i>
       </button>
       <div class="z-50 text-left ml-8">
-        <!-- Song Info -->
         <div class="text-3xl font-bold">{{ song.modified_name }}</div>
         <div v-if="song.genre">{{ song.genre }}</div>
       </div>
     </div>
   </section>
-  <!-- Form -->
   <section class="container mx-auto mt-6">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <!-- Comment Count -->
         <span class="card-title">Comments (15)</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
@@ -49,7 +45,6 @@
             Submit
           </button>
         </VeeForm>
-        <!-- Sort Comments -->
         <select
           v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
@@ -60,7 +55,6 @@
       </div>
     </div>
   </section>
-  <!-- Comments -->
   <ul class="container mx-auto">
     <li
       v-for="comment in sortedComments"
