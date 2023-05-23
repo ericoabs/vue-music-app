f94fc7889f115f07e11f913a346a88bcd1e618b2
Erico Alexandre <ericoabs25@gmail.com>
2023-05-23 20:36:11 -0300

    test: create unit test for the router

---
+++ src/components/__tests__/router.spec.ts
@@ -0,0 +1,25 @@
+import { it, expect, describe } from 'vitest'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
+import SongItem from '@/components/SongItem.vue'
+
+describe('Router', () => {
+  it('should render router link', () => {
+    const song = {
+      docID: 'test'
+    }
+
+    const wrapper = shallowMount(SongItem, {
+      propsData: { song },
+      global: {
+        components: {
+          'router-link': RouterLinkStub
+        }
+      }
+    })
+
+    expect(wrapper.findComponent(RouterLinkStub).props().to).toEqual({
+      name: 'song',
+      params: { id: song.docID }
+    })
+  })
+})
